d95c5a2d15db5f78b121cc48b901badb67dbbbaa
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-12 02:02:45 -0500

    firebase auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Auth from "./components/Auth/Auth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        FREE SPEECH 2025 SITE IS HERE -)))))
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                FREE SPEECH 2025 SITE IS HERE -)))))
+            </header>
+            <Auth/>
+        </div>
+    );
 }
+
 export default App;
---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDDDyOw0EgDP5_RAQqTv0y-oM4tJR9cnuA",
+    authDomain: "freespeech2025-46bc5.firebaseapp.com",
+    databaseURL: "https://freespeech2025-46bc5-default-rtdb.firebaseio.com",
+    projectId: "freespeech2025-46bc5",
+    storageBucket: "freespeech2025-46bc5.appspot.com",
+    messagingSenderId: "831583678552",
+    appId: "1:831583678552:web:74cff3c6bcf5ecdccb1a2d"
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
