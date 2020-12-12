28c151098bbd6c16d659f526200b050b38d6372c
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-12 16:52:10 -0500

    link auth

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -4,17 +4,59 @@ import {auth, db} from '../../utils/firebase'
 
 function Auth() {
     
-    const [isSubmitClicked, setIsSubmitClicked] = React.useState(false)
-    const [values, setValues] = React.useState({
+    const [isSubmitClicked, setIsSubmitClicked] = useState(false)
+    const [values, setValues] = useState({
         email: '',
         password: ''
     })
     
+    const actionCodeSettings = {
+        url: 'https://freespeech2025.com',
+        handleCodeInApp: true
+    };
+
+
+// Confirm the link is a sign-in with email link.
+    if (auth.isSignInWithEmailLink(window.location.href)) {
+        // Additional state parameters can also be passed via URL.
+        // This can be used to continue the user's intended action before triggering
+        // the sign-in operation.
+        // Get the email if available. This should be available if the user completes
+        // the flow on the same device where they started it.
+        let email = window.localStorage.getItem('emailForSignIn');
+        if (!email) {
+            // User opened the link on a different device. To prevent session fixation
+            // attacks, ask the user to provide the associated email again. For example:
+            email = window.prompt('Please provide your email for confirmation');
+        }
+        // The client SDK will parse the code from the link for you.
+        auth.signInWithEmailLink(email, window.location.href)
+            .then(function(result) {
+                // Clear email from storage.
+                window.localStorage.removeItem('emailForSignIn');
+                // You can access the new user via result.user
+                // Additional user info profile not available via:
+                // result.additionalUserInfo.profile == null
+                // You can check if the user is new or existing:
+                // result.additionalUserInfo.isNewUser
+            })
+            .catch(function(error) {
+                console.error(error);
+                // Some error occurred, you can inspect the code: error.code
+                // Common errors could be invalid email and invalid or expired OTPs.
+            });
+    }
+    
     useEffect(() => {
         if (isSubmitClicked) {
-            auth.createUserWithEmailAndPassword(values.email, values.password)
-                .then(credentials => console.log(credentials))
-                .catch((err) => console.log(err.code));
+            auth.sendSignInLinkToEmail(values.email, actionCodeSettings)
+                .then(function () {
+                    console.log('The link was successfully sent')
+                    window.localStorage.setItem('emailForSignIn', values.email);
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
             setIsSubmitClicked(false)
         }
     }, [isSubmitClicked])
@@ -32,7 +74,7 @@ function Auth() {
     return (
         <div className="authForm">
             <form className="form form-sign-up" name="form-signup" noValidate>
-                <h2 className="form__heading">Sign UP</h2>
+                <h2 className="form__heading">Sign UP With Email Link</h2>
                 <fieldset className="form__fields">
                     <label className="form__field-input">
                         <input
@@ -45,21 +87,6 @@ function Auth() {
                             maxLength="130"
                             required
                             autoComplete="username"
-                            onChange={handleChange}
-                        />
-                        <span className="form__field"/>
-                    </label>
-                    <label className="form__field-input">
-                        <input
-                            className="form__input form__input_position form__input-second-field"
-                            type="password"
-                            id="second-field-place"
-                            placeholder="Password"
-                            name="password"
-                            minLength="6"
-                            maxLength="20"
-                            required
-                            autoComplete="new-password"
                             onChange={handleChange}
                         />
                         <span className="form__field"/>
