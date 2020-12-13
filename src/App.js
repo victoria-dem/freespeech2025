8398e9151514f96a931ab8870e5eae5a325b371f
Victoria Dementyeva <videmy@gmail.com>
2020-12-13 10:42:49 -0500

    search begining

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Auth from "./components/Auth/Auth";
+import Form from "./components/Form/Form";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
                 FREE SPEECH 2025 SITE IS HERE -)))))
             </header>
             <Auth/>
+            <Form/>
         </div>
     );
 }
