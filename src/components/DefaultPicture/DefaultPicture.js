e6b097e4d5ba60b6f6474fff06ff0ad175295ba8
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-17 02:29:13 -0500

    petition

--- src/components/DefaultPicture/DefaultPicture.js
+++ src/components/DefaultPicture/DefaultPicture.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function DefaultPicture({url}){
-    console.log(url)
     return(
         <>
             <img className="petition-form__default-picture" src={url} alt={'картинка'}/>
--- src/components/PetitionDefaultPicture/PetitionDefaultPicVersionOne.js
+++ src/components/PetitionDefaultPicture/PetitionDefaultPicVersionOne.js
@@ -1,63 +1,43 @@
-// import React, {useEffect, useState} from 'react'
-// import "./petition-default-picture.css"
-// import {storage} from "../../utils/firebase";
-// import getDefaultPictures from "../../utils/defaultPictures";
-// import {v4 as uuidv4} from 'uuid';
-// import DefaultPicture from "../DefaultPicture/DefaultPicture"
-//
-// function PetitionDefaultPicture({isTextReadyToRender}) {
-//
-//     const [defaultPicUrls, setDefaultPicUrls] = useState([])
-//
-//     const picDataSet = getDefaultPictures()
-//
-//     useEffect(() => {
-//         console.log(picDataSet[0], picDataSet[1], picDataSet[2])
-//
-//         storage.ref(picDataSet[0])
-//             .getDownloadURL()
-//             .then(function (url) {
-//                 console.log(url);
-//                 const newUrl = [...defaultPicUrls, url]
-//                 setDefaultPicUrls(newUrl)
-//                 console.log('111111111111111111111111111111111111111111111111')
-//             })
-//             .catch(function (error) {
-//                 console.log("error encountered");
-//             });
-//
-//         storage.ref(picDataSet[1])
-//             .getDownloadURL()
-//             .then(function (url) {
-//                 console.log(url);
-//                 const newUrl = [...defaultPicUrls, url]
-//                 setDefaultPicUrls(newUrl)
-//                 console.log('22222222222222222222222222222222222221')
-//             })
-//             .catch(function (error) {
-//                 console.log("error encountered");
-//             });
-//
-//         storage.ref(picDataSet[2])
-//             .getDownloadURL()
-//             .then(function (url) {
-//                 console.log(url);
-//                 const newUrl = [...defaultPicUrls, url]
-//                 setDefaultPicUrls(newUrl)
-//                 console.log('3333333333333333333333333')
-//             })
-//             .catch(function (error) {
-//                 console.log("error encountered");
-//             });
-//     }, [])
-//
-//     console.log(defaultPicUrls.length);
-//
-//     return (
-//         <div className="petition-form__default-pictures">
-//             {defaultPicUrls.map((url) => <DefaultPicture key={uuidv4()} url={url}/>)}
-//         </div>
-//     )
-// }
-//
-// export default PetitionDefaultPicture
+import React, {useEffect, useState} from 'react'
+import "./petition-default-picture.css"
+import {storage} from "../../utils/firebase";
+import getDefaultPictures from "../../utils/defaultPictures";
+import {v4 as uuidv4} from 'uuid';
+import DefaultPicture from "../DefaultPicture/DefaultPicture"
+
+function PetitionDefaultPicture({isTextReadyToRender}) {
+    
+    const [defaultPicUrls, setDefaultPicUrls] = useState([])
+    const picDataSet = getDefaultPictures()
+    
+    
+    function funOne(picReef) {
+        storage.ref(picReef)
+            .getDownloadURL().then(function (url) {
+                console.log(url);
+                const newUrl = [...defaultPicUrls, url]
+                setDefaultPicUrls(newUrl)
+            })
+            .catch(function (error) {
+                console.log("error encountered");
+            });
+    }
+    
+    function fun() {
+        return Promise.all([funOne(picDataSet[0]), funOne(picDataSet[1]), funOne(picDataSet[2])])
+    }
+    
+    fun().then(
+        (value => {
+            console.log(value);
+        })
+    )
+    
+    return (
+        <div className="petition-form__default-pictures">
+            {defaultPicUrls.map((url) => <DefaultPicture key={uuidv4()} url={url}/>)}
+        </div>
+    )
+}
+
+export default PetitionDefaultPicture
