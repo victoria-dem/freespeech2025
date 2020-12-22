51934282f72c24f83afc3affb26ac6c2a0a80c11
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2020-12-22 17:52:49 +0300

    feat: другой вид карусели(адаптивный)

--- src/components/PetitionCardList/PetitionCardList.js
+++ src/components/PetitionCardList/PetitionCardList.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PetitionCard from "../PetitionCard/PetitionCard";
 import { v4 as uuidv4 } from 'uuid';
-import Carousel from 'react-spring-3d-carousel';
+//import Carousel from 'react-spring-3d-carousel';
+import Carousel from 'react-elastic-carousel';
 import { config } from 'react-spring';
-
 import './petition-card-list.css';
-import { useContext } from "react";
 
 const PetitionCardList = ({ petitions, onLikeClick, onDislikeClick,
-  onMyPetitionsChoose, onActualPetitionsChoose, isLoggedIn}) => {
-    const [isActButtonClicked, setIsActButtonClicked] = useState(false);
-    const [isMyButtonClicked, setIsMyButtonClicked] = useState(false);
+  onMyPetitionsChoose, onActualPetitionsChoose, isLoggedIn }) => {
+  const carousel = useRef();
+  const [isActButtonClicked, setIsActButtonClicked] = useState(true);
+  const [isMyButtonClicked, setIsMyButtonClicked] = useState(false);
   const handleMyPetitionsButtonClick = () => {
     onMyPetitionsChoose();
     setIsMyButtonClicked(true);
@@ -24,11 +24,11 @@ const PetitionCardList = ({ petitions, onLikeClick, onDislikeClick,
   }
 
   const isListEmpty = petitions.length === 0;
-
+  
   return (
     <div className="petition-card-list">
       <h2>
-        <a  id="petition-card-list"  name="petitions"/>
+        <a id="petition-card-list" name="petitions" />
         Инициативы
       </h2>
       <div className="petition-card-list__options">
@@ -38,18 +38,32 @@ const PetitionCardList = ({ petitions, onLikeClick, onDislikeClick,
           onClick={handleMyPetitionsButtonClick}>Мои инициативы</button>
       </div>
       {isListEmpty ? 'empty' :
+        <Carousel itemPosition='center' enableSwipe={true} ref={carousel}>
+          {
+            petitions.map((petition) => {
+              return (
+                <PetitionCard key={uuidv4()} petition={petition} onLikeClick={onLikeClick}
+                  onDislikeClick={onDislikeClick} isLoggedIn={isLoggedIn} />
+              )
+            })
+          }
+        </Carousel>
+      }
+      {/* {isListEmpty ? 'empty' :
         <Carousel slides={
           petitions.map((petition) => {
             return ({
               key: uuidv4(),
-              content: <PetitionCard petition={petition} onLikeClick={onLikeClick} onDislikeClick={onDislikeClick} isLoggedIn={isLoggedIn}/>
+              content: <PetitionCard petition={petition} onLikeClick={onLikeClick} 
+                onDislikeClick={onDislikeClick} isLoggedIn={isLoggedIn}/>
             }
             )
           })
         }
           showNavigation={true}
           animationConfig={config.slow}
-        />}
+        />} */}
+
     </div>
   );
 }
