7e34751c869634a04ef4a1c8098c4f50230f16e9
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-23 11:09:51 -0500

    use memo

--- src/components/PoemLine/PoemLine.js
+++ src/components/PoemLine/PoemLine.js
@@ -1,27 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
-function PoemLine({line, poemId}) {
+const PoemLine = ({line, poemId}) => {
     
-    const [isAnimated, setIsAnimated] = useState('false')
-    const [prevPoemId, setPrevPoemId] = useState(0)
-    
-    // console.log(isAnimated, prevPoemId, poemId, poemId===prevPoemId)
-    
-    useEffect(() => {
-        
-        prevPoemId===poemId && setIsAnimated(false)
-        
-    }, [poemId])
-    
-    useEffect(() => {
-        
-        prevPoemId!==poemId && setIsAnimated(true)
-        prevPoemId!==poemId && setPrevPoemId(poemId)
-        
-    }, [poemId])
+    const [prevPoemId, setPrevPoemId] = React.useState(0)
     
     
+    React.useEffect(()=>{
+        setPrevPoemId(poemId)
+    }, [])
+    
+    console.log('renderagain', poemId, prevPoemId)
     return <p className="petition-form__poem-line">{line}</p>
 }
-
-export default PoemLine;
+export default React.memo(PoemLine)
