3b7fa87d779970b9db15eda49aa3b9d87a153f79
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2020-12-25 01:15:25 +0300

    style: адаптив петиций 1ая итерация

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         Free Speach
       </p> */}
       <p className="footer__copyright">&copy; <span id="year">2025 </span>Free Speech</p>
-      <p className="footer__text">Горячо одобрено Министерством Свободы Слова</p>
+      <p className="footer__copyright">Горячо одобрено Министерством Свободы Слова</p>
     </div>
   );
 }
