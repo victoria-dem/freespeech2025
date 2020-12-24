404f4657a6449c14a3b12890cdac506f81c28733
Victoria Dementyeva <videmy@gmail.com>
2020-12-24 06:06:29 -0500

    fix bugs

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
     return (
         <div className="banner">
             <img className="banner__image" src={banner} alt="banner"/>
-            <h1 className="banner__title">Цензура больше не проблема, обубликуем петицию цитатой поэта-классика</h1>
+            <h1 className="banner__title">Цензура больше не проблема, опубликуем петицию цитатой поэта-классика</h1>
         </div>
     )
 }
