e454d87483377d50ff86f193e75914b866f13ece
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-25 14:55:02 -0500

    green button

--- src/components/PetitionSubmitBtn/PetitionSubmitBtn.js
+++ src/components/PetitionSubmitBtn/PetitionSubmitBtn.js
@@ -11,36 +11,56 @@ function PetitionSubmitBtn(props) {
         isLoaded,
         isPictureReady,
         isPetitionPublished,
-        isPetitionSubmitted
+        isPetitionSubmitted,
+        resetPublishState
     } = props
     
     const currentUser = useContext(CurrentUserContext);
-    const [petitionBtnTitle, setPetitionBtnTitle] = useState('Прояви инициативу')
+    const [petitionBtnTitle, setPetitionBtnTitle] = useState('Отправить')
     const [isSubmitBtnAvailable, setIsSubmitBtnAvailable] = useState(false)
     const [justPublished, setJustPublished] = useState(false)
+    
     
     const handleSubmitPetition = e => {
         e.preventDefault();
         getSubmitPetitionEvent(true)
     }
     
+    console.log(isPetitionPublished)
+    
     useEffect(() => {
-        if (!isTextReadyToRender && !isLoaded) {
-            if (justPublished) {
-                setPetitionBtnTitle('Инициатива на сайте')
+        // if (!isTextReadyToRender && !isLoaded) {
+        //     if (justPublished) {
+        //         setPetitionBtnTitle('Инициатива на сайте')
+        //         setTimeout(() => {
+        //             setJustPublished(false)
+        //             setPetitionBtnTitle('Прояви инициативу')
+        //         }, 3000)
+        //     }
+        // } else if (isTextReadyToRender && !isLoaded && !isPictureReady && !isPetitionPublished) {
+        //     setPetitionBtnTitle('А картинка где?')
+        // } else if (isTextReadyToRender && !isLoaded && isPictureReady && !isPetitionPublished && currentUser.uid) {
+        //     setPetitionBtnTitle('Готова к публикации')
+        // } else if (isLoaded) {
+        //     setPetitionBtnTitle('Публикуем ...')
+        // } else if (isPetitionPublished) {
+        //     setJustPublished(true)
+        // }
+        
+        if (isPetitionPublished) {
+            setJustPublished(true)
+            setPetitionBtnTitle('Публикуем...')
+            setTimeout(()=>{
+                setPetitionBtnTitle('Опубликовано')
                 setTimeout(() => {
                     setJustPublished(false)
-                    setPetitionBtnTitle('Прояви инициативу')
+                    setPetitionBtnTitle('Отправить')
+                    resetPublishState(false)
                 }, 3000)
-            }
-        } else if (isTextReadyToRender && !isLoaded && !isPictureReady && !isPetitionPublished) {
-            setPetitionBtnTitle('А картинка где?')
-        } else if (isTextReadyToRender && !isLoaded && isPictureReady && !isPetitionPublished && currentUser.uid) {
-            setPetitionBtnTitle('Готова к публикации')
-        } else if (isLoaded) {
-            setPetitionBtnTitle('Публикуем ...')
-        } else if (isPetitionPublished) {
-            setJustPublished(true)
+            }, 1000)
+
+        } else {
+            setPetitionBtnTitle('Отправить')
         }
     }, [isPetitionSubmitted, isTextReadyToRender, isPictureReady, isLoaded, isPetitionPublished])
     
@@ -56,13 +76,14 @@ function PetitionSubmitBtn(props) {
         <button
             type="submit"
             className={cn("petition-form__submit-btn",
-                {"petition-form__submit-btn_disabled": !isSubmitBtnAvailable},
-                {"petition-form__submit-btn_active" : isSubmitBtnAvailable})}
+                {"petition-form__submit-btn_disabled": !isSubmitBtnAvailable && !justPublished},
+                {"petition-form__submit-btn_active": isSubmitBtnAvailable && !justPublished},
+                {"petition-form__submit-btn_published": justPublished})}
             onClick={handleSubmitPetition}
             disabled={!isSubmitBtnAvailable}
         >
-            Отправить
-            {/*{petitionBtnTitle}*/}
+            {/*Отправить*/}
+            {petitionBtnTitle}
         </button>
     )
 }
