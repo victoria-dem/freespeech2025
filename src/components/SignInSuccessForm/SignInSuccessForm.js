bea802afb747f0010e4df24d24dca871c593d259
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-26 17:56:25 -0500

    new msg

--- src/components/SignInSuccessForm/SignInSuccessForm.js
+++ src/components/SignInSuccessForm/SignInSuccessForm.js
@@ -6,7 +6,10 @@ function SignInSuccessForm() {
     return (
         <div className='popup__success'>
             <img className="popup__success-image" src={letter} alt='letter'/>
-            <p className="popup__success-header">Подтвердите адрес электронной почты</p>
+            <p className="popup__success-header">Пожалуйста, проверьте вашу почту.
+            
+            </p>
+            <p className="popup__success-text popup__success-text_narrow">Вам отправлена ссылка для подтверждения регистрации.</p>
             <p className="popup__success-text popup__success-text_narrow">Министерство Свободы Слова оставляет за собой
                 право на любые действия в ответ на публикацию вашей инициативы.</p>
         </div>
