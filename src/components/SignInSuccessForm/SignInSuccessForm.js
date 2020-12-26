017e30f1377dc13825d0d96a6e7f976aca7ae400
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-26 17:13:22 -0500

    msg improve

--- src/components/SignInSuccessForm/SignInSuccessForm.js
+++ src/components/SignInSuccessForm/SignInSuccessForm.js
@@ -7,8 +7,8 @@ function SignInSuccessForm() {
         <div className='popup__success'>
             <img className="popup__success-image" src={letter} alt='letter'/>
             <p className="popup__success-header">Подтвердите адрес электронной почты</p>
-            <p className="popup__success-text popup__success-text_narrow">Ещё раз ставим вас в известность, что
-                Министерство Свободы Слова за свои действия не отвечает.</p>
+            <p className="popup__success-text popup__success-text_narrow">Министерство Свободы Слова оставляет за собой
+                право на любые действия в ответ на публикацию вашей инициативы.</p>
         </div>
     )
 }
