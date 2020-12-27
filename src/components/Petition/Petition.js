0fd25b41b82fae8a8d398ff1091642e44ac1a515
Mikhail Lisus <lisusmikhail@gmail.com>
2020-12-27 11:51:18 -0500

    anonimous user UX

--- src/components/Petition/Petition.js
+++ src/components/Petition/Petition.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import './petition.css'
 import PetitionForm from "../PetitionForm/PetitionForm";
 import PetitionPicture from "../PetitionPicture/PetitionPicture"
 import PetitionTextPreview from "../PetitionTextPreview/PetitionTextPreview";
 import PetitionSubmitBtn from "../PetitionSubmitBtn/PetitionSubmitBtn";
 import PetitionDefaultPictures from "../PetitionDefaultPictures/PetitionDefaultPictures"
-import { db, storage } from "../../utils/firebase";
-import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import {db, storage} from "../../utils/firebase";
+import {CurrentUserContext} from "../../contexts/CurrentUserContext";
 import PetitionSteps from "../PetitionSteps/PetitionSteps";
 
-function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
+function Petition({onAddPetition, nickname, handleAccountBtnClick}) {
     const currentUser = useContext(CurrentUserContext);
     const [poemText, setPoemText] = useState('')
     const [tagText, setTagText] = useState('')
@@ -22,8 +22,8 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
     const [url, setUrl] = useState('')
     const [isPublic, setIsPublic] = useState(false)
     const [isAnimationIn, setIsAnimationIn] = React.useState(false)
-    const [isDefaultPictureChosen, setIsDefaultPictureChosen] =useState(false)
-
+    const [isDefaultPictureChosen, setIsDefaultPictureChosen] = useState(false)
+    
     const handleDeletePicture = () => {
         if (url) {
             setUrl('')
@@ -33,33 +33,33 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
         // TODO: надо получить текущее имя файла и запустить удаление этого файла, если он не принадлежит к массиву заглушек
         // TODO: затем внутри then очистить объект pictureData setPictureData({}) и убрать эту очистку из предыдущего if
     }
-
+    
     const getPetitionTextData = (petitionTextData) => {
         setPoemText(petitionTextData.poemText)
         setTagText(petitionTextData.tagText)
         setIsTextReadyToRender(petitionTextData.isPetitionReady)
     }
-
+    
     const resetPublishState = (state) => {
         setIsPetitionPublished(state)
     }
-
+    
     useEffect(() => {
         if (isTextReadyToRender && poemText) {
             setIsAnimationIn(true)
-            setTimeout(()=>{
+            setTimeout(() => {
                 setIsAnimationIn(false)
             }, 3000)
         }
     }, [isTextReadyToRender, poemText])
-
-
-    const getPetitionPicData = ({ picRef, isPicUploaded }) => {
+    
+    
+    const getPetitionPicData = ({picRef, isPicUploaded}) => {
         setPictureData(picRef)
         setIsPictureReady(isPicUploaded)
         setIsPublic(false)
     }
-
+    
     const getDefaultPetitionPicData = (defaultPicName) => {
         if (defaultPicName) {
             console.log('default picture chosen')
@@ -73,11 +73,11 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
             setIsPublic(true)
         }
     }
-
+    
     const getSubmitPetitionEvent = (isBtnClicked) => {
         setIsPetitionSubmitted(isBtnClicked)
     }
-
+    
     useEffect(() => {
         if (isPictureReady) {
             const storagePic = storage.ref(pictureData.picFullPath);
@@ -91,7 +91,7 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
                 });
         }
     }, [pictureData])
-
+    
     // создание записи в db
     useEffect(() => {
         if (isPetitionSubmitted && currentUser.uid) {
@@ -115,32 +115,39 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
             db.collection("petitions")
                 .add(data)
                 .then(function (docRef) {
-                    onAddPetition({ data: data, id: docRef.id });
+                    onAddPetition({data: data, id: docRef.id});
                 }).then(function () {
                     setIsPetitionPublished(true)
                 })
                 .catch(function (error) {
                     console.error("Error adding document: ", error);
                 }).finally((() => {
-                    // TODO: подумать правильно ли то, что этот находится в finally а не в then
-                    // TODO: резон для этого в том, что я хочу чтобы даже если ошибка возникла мы
-                    //  TODO:все равно проресетили все стейты и были готовы к приему новой петиции
-                    setIsTextReadyToRender(false)
-                    setIsPictureReady(false)
-                    setUrl('')
-                    setPoemText('')
-                    setTagText('')
-                    setResetTextInputs(!resetTextInputs)
-                    setIsPetitionSubmitted(false)
-                }))
+                // TODO: подумать правильно ли то, что этот находится в finally а не в then
+                // TODO: резон для этого в том, что я хочу чтобы даже если ошибка возникла мы
+                //  TODO:все равно проресетили все стейты и были готовы к приему новой петиции
+                setIsTextReadyToRender(false)
+                setIsPictureReady(false)
+                setUrl('')
+                setPoemText('')
+                setTagText('')
+                setResetTextInputs(!resetTextInputs)
+                setIsPetitionSubmitted(false)
+            }))
         }
     }, [isPetitionSubmitted])
-
+    
     return (
         <section className="petition-form">
-            <h1 id="petition-form" className="petition-form__title">Создать инициативу</h1>
+            <div className="petition-form__title-area">
+                <h1 id="petition-form" className="petition-form__title">Создать инициативу</h1>
+                <p className="petition-form__subtitle">
+                    {!currentUser.uid && `Чтобы подать инициативу, войдите в `}
+                    <span  className="petition-form__subtitle-link" onClick={handleAccountBtnClick}>
+                        {!currentUser.uid && `аккаунт`}
+                    </span></p>
+            </div>
             <div className="petition-form__steps">
-                <PetitionSteps />
+                <PetitionSteps/>
             </div>
             <div className="petition-form__input">
                 <PetitionForm
@@ -158,7 +165,7 @@ function Petition({ onAddPetition, nickname, handleAccountBtnClick }) {
                 />
                 <PetitionDefaultPictures
                     getDefaultPetitionPicData={getDefaultPetitionPicData}
-                    isTextReadyToRender={isTextReadyToRender} />
+                    isTextReadyToRender={isTextReadyToRender}/>
             </div>
             <PetitionSubmitBtn
                 getSubmitPetitionEvent={getSubmitPetitionEvent}
